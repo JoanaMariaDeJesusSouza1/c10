ed28fa996f15c43cdf0500eca0059c1055f976fc
joana maria de jesus souza <98822804+JoanaMariaDeJesusSouza1@users.noreply.github.com>
2022-02-23 18:42:45 -0300

    Add files via upload

---
+++ c10 trex/Trex-Etapa-0-main/sketch.js
@@ -0,0 +1,51 @@
+
+
+
+//preload carrega as midías
+function preload(){
+ //animação do Trex
+  
+  //imagem do solo
+  
+
+}
+//setup faz aconfiguração
+function setup(){
+  createCanvas(600,200);
+  // criando as bordas
+ 
+  //crie um sprite de trex
+ 
+  // adicione dimensão e posição ao trex
+ 
+  //sprite do solo
+  
+ 
+}
+//draw faz o movimento, a ação do jogo
+function draw(){
+  background("gray");
+
+  
+
+
+  // dando velocidade ao solo
+  
+  // fazero trex pular
+   
+ //conferindo a rolagem do solo
+     
+ //coordenadas do mouse na tela
+   text("X: "+mouseX+"  / Y: "+mouseY,mouseX,mouseY);
+  
+ // chamando a  função de gravidade
+  
+  //colisão do trex com as bordas
+    
+
+  
+  drawSprites();
+
+}
+// função de gravidade
+
